3208e5055076e70cb8a4380cdba9bd46826652b7
SanchitNardekar <sanchit@motorway.co.uk>
2023-10-27 18:30:09 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PrTPredicting asteroid diameters from the Open Asteroid Dataset https://www.kaggle.com/datasets/basu369victor/prediction-of-asteroid-diameter. Toy repo to learn MLOps best practices.',
+    author='Sanchit Nardekar',
+    license='MIT',
+)
