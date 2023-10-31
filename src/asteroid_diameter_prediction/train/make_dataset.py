e9a5cf50e9cb3705c7943592aa16ccd1ce76b917
SanchitNardekar <sanchit@motorway.co.uk>
2023-10-31 22:55:36 +0000

    feat: dvc changes and updates to make_dataset

--- src/asteroid_diameter_prediction/train/make_dataset.py
+++ src/asteroid_diameter_prediction/train/make_dataset.py
@@ -1,30 +1,61 @@
-# # -*- coding: utf-8 -*-
-# import click
-# import logging
-# from pathlib import Path
-# from dotenv import find_dotenv, load_dotenv
+# src/asteroid-diameter-prediction/train/make_dataset.py
+
+import click
+import logging
+import os
+
+import pandas as pd
 
 
-# @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
-# def main(input_filepath, output_filepath):
-#     """ Runs data processing scripts to turn raw data from (../raw) into
-#         cleaned data ready to be analyzed (saved in ../processed).
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.info('making final data set from raw data')
+@click.command()
+@click.option('--reprocess', default=False, help='Runs process pipeline from scratch')
+def main(reprocess: bool = False):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+
+    input_path = '~/asteroid-diameter-prediction/data/Asteroid.csv'
+    output_path = '~/asteroid-diameter-prediction/data/Asteroid_processed.csv'
+
+    if reprocess or not os.path.exists(output_path):
+        df = pd.read_csv(input_path)
+
+        try: 
+            df.diameter = pd.to_numeric(df.diameter)
+        except ValueError as e:
+            print("ERROR:", e)
+
+        converted_vals = []
+        for idx, val in enumerate(df.diameter.tolist()):
+            if isinstance(val, str):
+                converted_vals.append(float(val))
+            else:
+                converted_vals.append(float(val))
+
+        df.diameter = converted_vals
+        df.diameter = pd.to_numeric(df.diameter)
+
+        df_numerical = df[[col for col, dtype in zip(df.columns, df.dtypes) if dtype != object]]
+
+        # Use imputer here
+        # df_cleaned = df[df["diameter"].notnull()]
+        # df_cleaned = df_cleaned[[col for col in df_cleaned.columns if (df_cleaned[col].count() / len(df_cleaned)) > 0.95]]
+
+        df_numerical.to_csv(output_path, index=False)
 
 
-# if __name__ == '__main__':
-#     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-#     # not used in this stub but often useful for finding various files
-#     project_dir = Path(__file__).resolve().parents[2]
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-#     # find .env automagically by walking up directories until it's found, then
-#     # load up the .env entries as environment variables
-#     load_dotenv(find_dotenv())
+    # not used in this stub but often useful for finding various files
+    # project_dir = Path(__file__).resolve().parents[2]
 
-#     main()
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    # load_dotenv(find_dotenv())
+
+    main()
