483c8af68bf745998e32f0d42c48aeb16505783a
SanchitNardekar <sanchit@motorway.co.uk>
2023-11-05 12:42:05 +0000

    refactor: make-dataset and processed data updates

--- src/asteroid_diameter_prediction/train/make_dataset.py
+++ src/asteroid_diameter_prediction/train/make_dataset.py
@@ -2,21 +2,64 @@
 
 import click
 from loguru import logger
+from pathlib import Path
 import os
 
 import pandas as pd
 
+
+def drop_negative(df):
+    """ Drops all rows with negative values in a column.
+
+        Args:
+            df = dataframe to be transformed
+
+        Returns:
+            Transfored dataframe
+    """
+    for col in df:
+        if df[col].dtype == 'object':
+            continue
+        else:
+            if df[col][df[col] < 0].count() > 0:
+                df.drop(df[df[col] < 0].index, axis=0, inplace=True)
+            else:
+                continue
+    return df
+
+
+def VIF_calc(df):
+    """ Calculate VIF (Variance inflation factor) for all columns in a dataframe.
+
+        Args:
+            df = dataframe to calculate VIF
+
+        Returns:
+            pd.Series with df.columns as index and VIF as values.
+    """
+    from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+    aux_df = df.assign(const=1)
+    vif = pd.Series([variance_inflation_factor(aux_df.values, i) 
+               for i in range(len(aux_df.columns))], 
+              index=aux_df.columns)
+    return vif
+
+
 @click.command()
 @click.option('--reprocess', default=False, help='Runs process pipeline from scratch')
 def main(reprocess: bool = False):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """ Runs data processing scripts to turn raw data from into
+        cleaned data ready to be analyzed.
+
+        Args:
+            reprocess, bool = TRUE if reprocessing raw data from scratch, else False
     """
 
-    input_path = '~/asteroid-diameter-prediction/data/Asteroid.csv'
-    output_path = '~/asteroid-diameter-prediction/data/Asteroid_processed.csv'
+    input_path = '/Users/sanchitnardekar/asteroid-diameter-prediction/data/Asteroid.csv'
+    output_path = '/Users/sanchitnardekar/asteroid-diameter-prediction/data/Asteroid_processed.csv'
 
-    if reprocess:
+    if reprocess or not Path(output_path).exists():
         df = pd.read_csv(input_path)
 
         logger.info('Processing data...')
@@ -28,16 +71,24 @@ def main(reprocess: bool = False):
             else:
                 converted_vals.append(float(val))
 
+        # Convert response to numerical
         df.diameter = converted_vals
         df.diameter = pd.to_numeric(df.diameter)
 
-        df_numerical = df[[col for col, dtype in zip(df.columns, df.dtypes) if dtype != object]]
+        # Drop columns with more than 5% of NULLs and rows with NULLs TODO: replace with median imputer
+        df = df[[col for col in df.columns if (df[col].count() / len(df)) > 0.95]]
+        df = df.dropna()
 
-        # Use imputer here
-        # df_cleaned = df[df["diameter"].notnull()]
-        # df_cleaned = df_cleaned[[col for col in df_cleaned.columns if (df_cleaned[col].count() / len(df_cleaned)) > 0.95]]
+        # Drop rows with negative values
+        df = drop_negative(df)
 
-        df_numerical.to_csv(output_path, index=False)
+        # Get only numeric columns
+        df = df[[col for col, dtype in zip(df.columns, df.dtypes) if dtype != object]]
+
+        # Get the variables with the least multi-collinearity
+        df = df[[col for col, vif in zip(df.columns, VIF_calc(df).values) if vif < 5]]
+
+        df.to_csv(output_path, index=False)
         logger.success(f'Created output file @ {output_path}')
     else:
         logger.info("Output file already exists.")
@@ -45,8 +96,6 @@ def main(reprocess: bool = False):
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
     # project_dir = Path(__file__).resolve().parents[2]
